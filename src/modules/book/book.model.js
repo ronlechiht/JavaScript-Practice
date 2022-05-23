10e9b7ef8127481bbccec615807555200ec9a586
Le Chi Ron <ronlechi1998ht@gmail.com>
2022-05-23 22:17:17 +0700

    implement book model

--- src/modules/book/book.model.js
+++ src/modules/book/book.model.js
@@ -1,3 +1,30 @@
 export class BookModel {
-  constructor(){}
+  constructor(){
+    this.books = [
+      {id: 1, isbnBook: 'ISBN-13',  nameBook: 'Ada Lovelace', authorBook: 'Ada Lovelace', issuedDate: '01/01/1900'}
+    ]
+  }
+
+  addBook(id,isbn,name,author,date) {
+    const book = {
+      id: this.books.length > 0 ? this.books[this.books.length - 1].id + 1:1,
+      isbnBook: isbn,
+      nameBook: name,
+      authorBook: author,
+      issuedDate: date,
+    }
+
+    this.books.push(book)
+  }
+
+  editBook(id,updatedIsbn, updatedName, updatedAuthor, updatedDate) {
+    this.books = this.books.map((book) =>
+      book.id === id ? {id: book.id, isbnBook: updatedIsbn, nameBook: updatedName, 
+      authorBook: updatedAuthor, issuedDate: updatedDate} : book,
+    )
+  }
+
+  deleteBook(id) {
+    this.books = this.books.filter((book) => book.id !== id)
+  }
 }
